edab393f8277e526f838b5aa497c530f9c0cca48
Shlomi <shlomicher@users.noreply.github.com>
2025-12-20 03:47:12 +0200

    Implemented unary minus, multiplication operators, equality operators, and the Frobenius norm ,I finished my part of the matrix implementation.

--- wet/Matrix.h
+++ wet/Matrix.h
@@ -4,6 +4,10 @@
 
 #ifndef WET_MATRIX_H
 #define WET_MATRIX_H
+#include <iosfwd>
+#include <ostream>
+#include <cmath>
+
 
 
 class Matrix {
@@ -24,8 +28,22 @@ public:
     int* getMatrix() const;
 
     Matrix(const Matrix& other);
+
     Matrix& operator=(const Matrix& other);
+
+    int& operator()(unsigned int row, unsigned int column);
+    const int& operator()(unsigned int row, unsigned int column) const;
+    Matrix operator-()const;
+    Matrix& operator*=(int n);
+    friend bool operator==(const Matrix& matrix1, const Matrix& matrix2);
+    double frobenius()const;
 };
 
+std::ostream& operator<<(std::ofstream& os, const Matrix& matrix);
+
+Matrix operator*(int n, const Matrix& matrix);
+Matrix operator*(const Matrix& matrix, int n);
+
+bool operator!=(const Matrix& matrix1, const Matrix& matrix2);
 
 #endif //WET_MATRIX_H
